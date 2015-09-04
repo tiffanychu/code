b12e65015c856f460c4999ca1f60b8cf6a2457b0
tiffanychu <lunabella1001@gmail.com>
2015-09-04 09:18:45 +0000

    message

---
+++ fresh_batch/ruby/first_app.rb
@@ -0,0 +1,70 @@
+
+#INTRO TO RUBY
+#integers #1, 2, 3, 4, 5 floats: 1.3, 2.5, 6.7, etc.
+#puts 4 + 3
+#puts "Hello, World!"
+#puts 5/3.to_f
+#turn into float --> .to_f
+#chaining --> chain methods together
+#variables --> store data; stores strings, integers, and floats
+#IMPORTANT: gets.chomp
+
+#puts "Hello, World!" + " My Name is Tiffany"
+#METHOD - makes the computer do something
+#puts "hello world".upcase
+#.length, .reverse, downcase, smallcase, capitalize
+#puts "hello world".reverse.upcase #chain methods together
+#capitalizing words means something different, variables should not be capitalized
+
+#VARIABLES; stores data; doesn't have to be a letter, can be words
+#number_of_boxes = 5
+#number_of_cookies = 4
+
+#total_cookies = puts number_of_boxes*number_of_cookies
+
+#puts total_cookies
+
+# puts "Hey, Thanks for buying from Fresh Batch.  What is your name?"
+
+# sign = " bought a cookie!"
+# name = gets.chomp
+
+# puts name + sign
+
+number = 1.to_f
+
+#a = 25
+#b = 36
+#puts a*b
+
+#band = "The Beatles"
+#puts "My favorite band is " + band
+
+
+# artist_first = "NiCKi"
+# artist_last = "MiNaj"
+
+# puts "When I listen to " + artist_first.upcase + " " + artist_last.downcase + " I feel like my brain is melting."
+
+# name = "Bill"
+#age = 16
+# location = "New York"
+
+# puts "Hi. I'm " + name.upcase + ". I'm from " + location.swapcase + " and in ten years I'm going to be " + (age+10).to_s
+
+x = 34679
+y = 566
+z = 47
+
+a = puts x*y*z
+puts a
+
+b = puts x+y+z
+puts b
+
+puts a-b
+
+
+
+
+
